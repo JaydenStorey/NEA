86bce80525141c6a9ba12c6962ce9d4e11f04231
JaydenStorey <19knowl_L@students.accordsixth.co.uk>
2025-05-13 14:43:35 +0100

    Move to CustomTkinter

--- NEA.py
+++ NEA.py
@@ -1,15 +1,19 @@
 import tkinter as tk
+import customtkinter as ctk
 
 class NEAGui:
     
     def __init__(self):
-        self.root = tk.Tk()
+        
+        ctk.set_appearance_mode("System")
+        
+        self.root = ctk.CTk()
         self.windowheight = 480
         self.windowwidth = 720
         self.root.geometry(f"{self.windowwidth}x{self.windowheight}")
         
         # Container stores the pages
-        self.container = tk.Frame(self.root) # To hold the central frame
+        self.container = ctk.CTkFrame(self.root) # To hold the central frame
         self.container.place(relx=0.5, rely=0.2, anchor="center", y = 50)
         
         self.pages = {}
@@ -51,27 +55,27 @@ class LoginPage(tk.Frame):
         
         self.controller = controller
                 
-        self.logintext = tk.Label(self, text = "Login", font = ('Arial, 40'))
+        self.logintext = ctk.CTkLabel(self, text = "Login", font = ('Arial', 40))
         self.logintext.pack()
         # email entry
         
-        self.emailentrylabel = tk.Label(self, text = "Enter Email Address", font = ('Arial, 15'))
+        self.emailentrylabel = ctk.CTkLabel(self, text = "Enter Email Address", font = ('Arial', 15))
         self.emailentrylabel.pack()
         
-        self.emailentry = tk.Entry(self, font = ('Arial, 15'), insertbackground = "grey", width = 45,)
+        self.emailentry = ctk.CTkEntry(self, font = ('Arial', 15), width = 45,)
         self.emailentry.pack()
         
         # password entry
         
-        self.passwordentrylabel = tk.Label(self, text = "Enter Password", font = ('Arial, 15'))
+        self.passwordentrylabel = ctk.CTkLabel(self, text = "Enter Password", font = ('Arial', 15))
         self.passwordentrylabel.pack()
 
-        self.passwordentry = tk.Entry(self, font = ('Arial, 15'), insertbackground = "grey", width = 45, show = "*")
+        self.passwordentry = ctk.CTkEntry(self, font = ('Arial', 15), width = 45, show = "*")
         self.passwordentry.pack()
         
         # login button
         
-        self.loginbutton = tk.Button(self, font = ('Arial, 30'), width = 5, text = "Login", bg = "light grey", command = lambda: self.CheckLoginConditions(self.emailentry.get(),self.passwordentry.get()))
+        self.loginbutton = ctk.CTkButton(self, font = ('Arial', 30), width = 5, text = "Login", fg_color = "light grey", command = lambda: self.CheckLoginConditions(self.emailentry.get(),self.passwordentry.get()))
         self.loginbutton.pack(pady = 10)
         
     def CheckLoginConditions(self,email,password):
@@ -87,10 +91,10 @@ class StartPage(tk.Frame):
         
         self.controller = controller
         
-        self.title = tk.Label(self, text = "Hot Stuff", font = ('Arial, 50'), anchor = "center")
+        self.title = ctk.CTkLabel(self, text = "Hot Stuff", font = ('Arial', 50), anchor = "center")
         self.title.pack(anchor = "center")
         
-        self.loginbutton = tk.Button(self, text = "Log In", command = lambda: controller.ShowPage(LoginPage))
+        self.loginbutton = ctk.CTkButton(self, text = "Log In", command = lambda: controller.ShowPage(LoginPage))
         self.loginbutton.pack()
         
 NEAGui()
